33a2f947a2f7e33c7d5015c4ca19be4084988baa
AndyGayar <andresgayarromero@gmail.com>
2025-02-24 16:01:15 +0100

    partes 1 y 2

--- examen.js
+++ examen.js
@@ -1,11 +1,13 @@
-document.getElementById('alertButton').addEventListener('click', function() {
-    alert('¡Hola! Este es un mensaje de saludo.');
-});
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('alertButton').addEventListener('click', function() {
+        alert('¡Hola! Este es un mensaje de saludo.');
+    });
 
-document.getElementById('dataForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const formDataDiv = document.getElementById('formData');
-    formDataDiv.innerHTML = `<p>Nombre: ${name}</p><p>Correo Electrónico: ${email}</p>`;
+    document.getElementById('dataForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const formDataDiv = document.getElementById('formData');
+        formDataDiv.innerHTML = `<p>Nombre: ${name}</p><p>Correo Electrónico: ${email}</p>`;
+    });
 });
